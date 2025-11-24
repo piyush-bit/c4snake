d402de3e504d587558862e08d1043aa6637587bd
Piyush Kumar <mr.piyush.cv@gmail.com>
2025-11-24 23:43:48 +0530

    pov of the snake

--- test.c
+++ test.c
@@ -11,7 +11,7 @@ const char *FILL100 = "█";
 const char *FILL75 = "▓";
 const char *FILL50 = "▒";
 const char *FILL25 = "░";
-const char *FILL0 = " ";
+const char *FILL0 =  " ";
 
 const char *CLEAR = "\033[2J";
 const char *RESET_CURSOR = "\033[H";
@@ -108,6 +108,9 @@ char dir = 'd';
         for (int j = 0; j < cols; j++) {
             char* element ;
             switch(screen[i][j]) {
+                case -1:
+                    printf("%s%s", FILL50,FILL50);
+                    break;
                 case 0:
                     printf("%s%s", FILL25,FILL25);
                     break;
@@ -115,11 +118,29 @@ char dir = 'd';
                     printf("%s%s", FILL100,FILL100);
                     break;
                 case 2:
-                    printf("%s%s", FILL50,FILL50);
+                    printf("%s%s", FILL50,FILL75);
                     break;
             }
         }
         printf("\n");
+    }
+}
+
+void get_pov(char screen[30][70], char pov[30][70], int x, int y){
+    int x_offset = x - 35; 
+    int y_offset = y - 15; 
+
+    for(int i = 0; i < 30; i++){
+        for(int j = 0; j < 70; j++){
+            int world_row = i + y_offset;
+            int world_col = j + x_offset;
+
+            if(world_row < 0 || world_row >= 30 || world_col < 0 || world_col >= 70){
+                pov[i][j] = -1; 
+            }else{
+                pov[i][j] = screen[world_row][world_col];
+            }
+        }
     }
 }
 
@@ -136,15 +157,17 @@ int main() {
     pthread_t t;
     pthread_create(&t, NULL, loop, NULL);
     char screen[30][70];
+    char pov[30][70];
     memset(screen, 0, sizeof(screen));
-    // for(int i = 0; i < 30; i++){
-    //     screen[i][0] = 1;
-    //     screen[i][69] = 1;
-    // }
-    // for(int i = 0; i < 70; i++){
-    //     screen[0][i] = 1;
-    //     screen[29][i] = 1;
-    // }
+    memset(pov, -1, sizeof(pov));
+    for(int i = 0; i < 30; i++){
+        screen[i][0] = 1;
+        screen[i][69] = 1;
+    }
+    for(int i = 0; i < 70; i++){
+        screen[0][i] = 1;
+        screen[29][i] = 1;
+    }
     struct snake s = {10, 10};
     s.next = &(struct snake){10, 9, NULL};
     s.next->next = NULL;
@@ -176,6 +199,7 @@ int main() {
         if(new_head->x < 0 || new_head->x >= 70 || new_head->y < 0 || new_head->y >= 30){
             break;
         }
+        get_pov(screen, pov, new_head->x, new_head->y);
         if(new_head->x == food.x && new_head->y == food.y){
             score++;
             screen[food.y][food.x] = 1;
@@ -192,9 +216,9 @@ int main() {
             screen[food.y][food.x] = 2;
         }
         
-        render(30, 70, screen);
+        render(30, 70, pov);
     }
     printf("Game Over :%d\n", score);
-    pthread_join(t, NULL);
+    // pthread_join(t, NULL);
   return 0;
 }
